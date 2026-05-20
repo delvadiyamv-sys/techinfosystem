c560f90801319ff5b53e79b35f5327ff3c183e9d
MV Patel <delvadiyamv@gmail.com>
2026-05-20 15:11:01 +0530

    change

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('Hello, World!');
+// This is a simple JavaScript file that logs "Hello, World!" to the console. You can run this code in a JavaScript environment, such as a web browser's developer console or a Node.js environment.
+// To run this code in a web browser, simply copy and paste it into the console and press Enter. You should see "Hello, World!" printed in the console.
+// To run this code in Node.js, save it to a file named index.js and then run the command `node index.js` in your terminal. You should see "Hello, World!" printed in the terminal.
+    
