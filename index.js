3a3c8999ee14f52957b4d988b27ec3a6831c57ec
MV Patel <delvadiyamv@gmail.com>
2026-05-20 15:15:50 +0530

    sfd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Hello, my World!');
-// This is a simple JavaScript file that logs "Hello, World!" to the console. You can run this code in a JavaScript environment, such as a web browser's developer console or a Node.js environment.
-// To run this code in a web browser, simply copy and paste it into the console and press Enter. You should see "Hello, World!" printed in the console.
-// To run this code in Node.js, save it to a file named index.js and then run the command `node index.js` in your terminal. You should see "Hello, World!" printed in the terminal.
+console.log('Hello, my tech World!');
+// This is a simple JavaScript file that logs "Hello, my tech World!" to the console. You can run this code in a JavaScript environment, such as a web browser's developer console or a Node.js environment.
+// To run this code in a web browser, simply copy and paste it into the console and press Enter. You should see "Hello, my tech World!" printed in the console.
+// To run this code in Node.js, save it to a file named index.js and then run the command `node index.js` in your terminal. You should see "Hello, my tech World!" printed in the terminal.
