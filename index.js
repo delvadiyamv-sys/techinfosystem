c7bf2ee7a40cf53d7327ff6f119e03937d73e489
MV Patel <delvadiyamv@gmail.com>
2026-05-20 15:46:55 +0530

    success

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 console.log('Hello, my tech World!');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const post = require('./Models/adminPost');
+const PORT = process.env.PORT || 3000;
+//var { Server } = require('socket.io');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+var io = require('socket.io')(http) ;
+
+
+
+
+const adminRouter = require('./Routes/admin_route');
+const userRouter = require('./Routes/user_route');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+//connect to the database if needed 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://delvadiyamv:mvd246@blogdata.d2h8lsr.mongodb.net/?retryWrites=true&w=majority&appName=blogdata')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+//const { objectID } = require('mongodb');
+
+app.use('/', adminRouter);
+app.use('/', userRouter);
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+  socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  socket.on('newPost', (data) => {
+    console.log('New post created:', data);
+    socket.broadcast.emit('newPost', data);
+    //io.emit('newPost', data); 
+
+  });
+  
+  socket.on('commentAdded', function (commentData) {
+    io.emit('commentAdded', commentData);
+    console.log('New comment added:', commentData);
+  });
+ 
+socket.on('updateViews', async (postId) => {
+    console.log('Post viewed:', postId);
+    var data = await post.findOneAndUpdate({ _id: postId }, { $inc: { Views: 1 } }, { new: true });
+    socket.broadcast.emit('viewsUpdated', data);
+  });
+});
+
+
+
+http.listen(PORT, () => {
+  console.log(`my new blog node Server is running on http://localhost:${PORT}`);
+});
+module.exports = app;
+//module.exports = io; hjhgjhgj
+
